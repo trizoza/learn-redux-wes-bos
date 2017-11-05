b8c5f0e786eaabfab14b709f5b7e151c18b5fb5e
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-11-05 18:54:58 +0000

    first 12 vids

---
+++ client/reducers/posts.js
@@ -0,0 +1,18 @@
+// reducers take in 2 information
+// 1.the action, info what happened
+// 2.copy of current state
+
+export default function posts (state = [], action) {
+  switch (action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('Incrementing likes!!!')
+      const i = action.index
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)
+      ]
+    default:
+      return state
+  }
+}
